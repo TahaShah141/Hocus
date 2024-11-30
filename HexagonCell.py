f3ca28f9110c23c95ace626179f6b2aed0a06b4e
Taha Shah <tahashah411@gmail.com>
2024-11-30 18:37:33 +0500

    connect hexagons

--- HexagonCell.py
+++ HexagonCell.py
@@ -8,6 +8,33 @@ class HexagonCell:
     self.overlapSide = -1
     self.nodes = [[Node(self, [d, D]) for d in range(D, 6)] for D in range(6)]
   
+  def getNode(self, dimension, otherDimension=None):
+    if (otherDimension == None):
+      otherDimension = dimension
+      
+    return self.nodes[min(dimension, otherDimension)][max(dimension, otherDimension)]
+    
+  
+  def connectHexagon(self, other, dimension): 
+    selfNode = None
+    if (self.neighbors[dimension] == None):
+      selfNode = self.getNode(dimension)
+    else:
+      otherSide = getHexagonSide(other)
+      selfNode = self.getNode(dimension, otherSide)
+    
+    otherNode = None
+    if (other.neighbors[dimension] == None):
+      otherNode = other.getNode(dimension)
+    else:
+      selfSide = other.getHexagonSide(self)
+      otherNode = other.getNode(otherSide, selfSide)
+    
+    selfNode.connectNode(dimension, otherNode)
+   
+    def getHexagonSide(self, other):
+      return self.neighbors.index(other)
+    
   def addNeighbor(self, side, neighbor):
     self.neighbors[side] = neighbor
     neighbor.neighbors[getOppositeSide(side)] = self
